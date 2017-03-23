432de0cfdf60ae066dc3eea0e3b7fb904bde661e
danaswanstrom <danaswanstrom@gmail.com>
2017-03-23 19:21:48 +0000

    New sh script

--- CanvasCLI_partial.sh
+++ CanvasCLI_partial.sh
@@ -1,6 +1,4 @@
 sudo docker run -ti --rm -e CD_API_KEY=$CD_API_KEY -e CD_API_SECRET=$CD_API_SECRET \
--v $(pwd):/canvas_data -d \
-canvas_cli_docker \
-/bin/sh \
-sync_some_unpack.sh
+-v $(pwd):/canvas_data \
+canvas_cli_docker 
  
